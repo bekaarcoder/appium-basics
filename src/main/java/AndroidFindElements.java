9c64bc3f0ba7c309783092828f8f20ef586e5b02
Shashank <s_shashank@outlook.com>
2021-05-09 23:32:02 +0530

    android locator stategies

---
+++ src/main/java/AndroidFindElements.java
@@ -0,0 +1,30 @@
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.FindsByAndroidUIAutomator;
+import io.appium.java_client.MobileElement;
+
+import java.net.MalformedURLException;
+
+public class AndroidFindElements {
+
+    public static void main(String[] args) throws MalformedURLException {
+        AppiumDriver driver = CreateDriverSession.initializeDriver("Android");
+
+        MobileElement myElement = (MobileElement) driver.findElementByAccessibilityId("Accessibility");
+        System.out.println(myElement.getText());
+
+        myElement = (MobileElement) driver.findElementsById("android:id/text1").get(1);
+        System.out.println(myElement.getText());
+
+        myElement = (MobileElement) driver.findElementsByClassName("android.widget.TextView").get(2);
+        System.out.println(myElement.getText());
+
+        myElement = (MobileElement) driver.findElementByXPath("//android.widget" +
+                ".TextView[@content-desc=\"Accessibility\"]");
+        System.out.println(myElement.getText());
+
+        myElement = (MobileElement) ((FindsByAndroidUIAutomator) driver).findElementByAndroidUIAutomator("new " +
+                "UiSelector().text(\"Accessibility\")");
+        System.out.println(myElement.getText());
+    }
+
+}
--- src/main/java/CreateDriverSession.java
+++ src/main/java/CreateDriverSession.java
@@ -8,28 +8,31 @@ import java.net.URL;
 
 public class CreateDriverSession {
 
-    public static void main(String[] args) throws MalformedURLException {
+    public static AppiumDriver initializeDriver(String platformName) throws MalformedURLException {
         DesiredCapabilities caps = new DesiredCapabilities();
-        caps.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        caps.setCapability(MobileCapabilityType.PLATFORM_NAME, platformName);
+        caps.setCapability("newCommandTimeout", 300);
+
+        URL url = new URL("http://0.0.0.0:4723/wd/hub");
+
         caps.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel_3");
         caps.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
         caps.setCapability(MobileCapabilityType.UDID, "emulator-5554");
 
+
         // Launch Android Emulator Automatically
-        caps.setCapability("avd", "Pixel_3");
-        caps.setCapability("avdLaunchTimeout", 180000);
+//        caps.setCapability("avd", "Pixel_3");
+//        caps.setCapability("avdLaunchTimeout", 180000);
 
         // Installs the apk in the device
-//        String appUrl = System.getProperty("user.dir") + "\\src\\main\\resources\\ApiDemos-debug.apk";
-//        caps.setCapability(MobileCapabilityType.APP, appUrl);
+        String appUrl = System.getProperty("user.dir") + "\\src\\main\\resources\\ApiDemos-debug.apk";
+        caps.setCapability(MobileCapabilityType.APP, appUrl);
 
         // Opens the existing application in the device
-        caps.setCapability("appPackage", "io.appium.android.apis");
-        caps.setCapability("appActivity", "io.appium.android.apis.ApiDemos");
+//        caps.setCapability("appPackage", "io.appium.android.apis");
+//        caps.setCapability("appActivity", "io.appium.android.apis.ApiDemos");
 
-        URL url = new URL("http://0.0.0.0:4723/wd/hub");
-
-        AppiumDriver driver = new AndroidDriver(url, caps);
+        return new AndroidDriver(url, caps);
     }
 
 }
